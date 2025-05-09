b6ce9563e715003e3c33c003f4b47d0769d4fa62
FlorianFleisch <florian.fleisch@student.htldornbirn.at>
2025-05-09 13:52:03 +0200

    finished_base_Strukure

---
+++ 3aWI_Projekt/Database/AppDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using _3aWI_Projekt.Models;
+
+namespace _3aWI_Projekt.Database;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<School> Schools => Set<School>();
+    public DbSet<Student> Students => Set<Student>();
+    public DbSet<Classroom> Classrooms => Set<Classroom>();
+
+    public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt) { }
+
+    protected override void OnModelCreating(ModelBuilder mb)
+    {
+        // ---------- Primärschlüssel ----------
+        mb.Entity<School>().HasKey(s => s.Id);
+        mb.Entity<Student>().HasKey(s => s.Id);
+        mb.Entity<Classroom>().HasKey(c => c.Id);
+
+        // ---------- Beziehungen ----------
+        mb.Entity<School>()
+          .HasMany(s => s.Students)
+          .WithOne()
+          .OnDelete(DeleteBehavior.Cascade);
+
+        mb.Entity<School>()
+          .HasMany(s => s.Classrooms)
+          .WithOne()
+          .OnDelete(DeleteBehavior.Cascade);
+
+        // Classroom erbt von School → Rekursion verhindern
+        mb.Entity<Classroom>()
+          .Ignore(c => c.Classrooms)
+          .Ignore(c => c.Students);  // falls doppelt vorhanden
+    }
+}
---
+++ 3aWI_Projekt/Models/Classroom.cs
@@ -0,0 +1,27 @@
+﻿using _3aWI_Projekt.Models;
+
+public class Classroom : School
+{
+    public string Size { get; set; }
+    public int NumberOfSeats { get; set; }
+    public bool HasCynap { get; set; }
+
+    public Classroom(string size, int numberOfSeats, bool hasCynap)
+    {
+        Size = size;
+        NumberOfSeats = numberOfSeats;
+        HasCynap = hasCynap;
+    }
+
+    public void AddStudent(Student student)
+    {
+        if (Students.Count < NumberOfSeats)
+        {
+            Students.Add(student);
+        }
+        else
+        {
+            // keine Plätze mehr frei
+        }
+    }
+}
