d1acbe2c0ed7f505e6617c3f8217b2abb3d88685
FlorianFleisch <144995166+FlorianFleisch@users.noreply.github.com>
2025-06-04 15:18:44 +0200

    Add endpoints for school stats

--- 3aWI_Projekt/Models/School.cs
+++ 3aWI_Projekt/Models/School.cs
@@ -14,4 +14,65 @@ public class School
     }
 
     protected School() { }
+
+    public void AddStudent(Student student)
+    {
+        Students.Add(student);
+    }
+
+    public void AddClassroom(Classroom classroom)
+    {
+        Classrooms.Add(classroom);
+    }
+
+    public int GetNumberOfStudents()
+    {
+        return Students.Count;
+    }
+
+    public (int male, int female) GetMaleAndFemaleStudentCount()
+    {
+        int maleCount = Students.Count(s => s.Gender == Person.Genders.m);
+        int femaleCount = Students.Count(s => s.Gender == Person.Genders.w);
+        return (maleCount, femaleCount);
+    }
+
+    public int GetNumberOfClassrooms()
+    {
+        return Classrooms.Count;
+    }
+
+    public double GetAverageAge()
+    {
+        return Students.Count > 0 ? Students.Average(s => s.Age) : 0;
+    }
+
+    public List<Classroom> GetClassroomsWithCynap()
+    {
+        return Classrooms.Where(c => c.Cynap).ToList();
+    }
+
+    public int GetNumberOfClasses()
+    {
+        return Students.Select(s => s.SchoolClass).Distinct().Count();
+    }
+
+    public Dictionary<string, int> GetClassStudentCounts()
+    {
+        return Students.GroupBy(s => s.SchoolClass.ToString())
+                       .ToDictionary(g => g.Key, g => g.Count());
+    }
+
+    public double GetFemalePercentageInClass(string className)
+    {
+        var classStudents = Students.Where(s => s.SchoolClass.ToString() == className).ToList();
+        int femaleCount = classStudents.Count(s => s.Gender == Person.Genders.w);
+        return classStudents.Count > 0 ? (double)femaleCount / classStudents.Count * 100 : 0;
+    }
+
+    public bool CanClassFitInRoom(string className, Classroom room)
+    {
+        int classSize = Students.Count(s => s.SchoolClass.ToString() == className);
+        return room.Seats >= classSize;
+    }
 }
