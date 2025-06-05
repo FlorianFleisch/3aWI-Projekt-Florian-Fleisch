1eae732a951a037641dc6dfc2146964597e1963d
FlorianFleisch <florian.fleisch@student.htldornbirn.at>
2025-06-05 21:28:41 +0200

    fixed_DbContext

--- 3aWI_Projekt/Program.cs
+++ 3aWI_Projekt/Program.cs
@@ -1,3 +1,6 @@
+using _3aWI_Projekt.Database;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors(options =>
@@ -12,9 +15,16 @@ builder.Services.AddCors(options =>
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<_3aWI_Projekt.Database.AppDbContext>();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
 
 if (app.Environment.IsDevelopment())
 {
